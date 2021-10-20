e16d2292ee4dfaa83e4a7997cd8974fb7721d9cb
Étienne <66807171+EtienneRld@users.noreply.github.com>
2021-10-20 16:32:18 +0200

    Update tri-selectif.java

--- tri-selectif/tri-selectif.java
+++ tri-selectif/tri-selectif.java
@@ -5,17 +5,27 @@ public class Main
     public static void main(String[] args)
     {
         int[] tableau = { 47, 12, -34, 86, -2 }; /* Déclaration du tableau */
-        int min, temp; /* Déclaration des variables */
+        int min, temp, comparaison, affectation; /* Déclaration des variables */
 
         for (int a = 0; a < tableau.length; a++) /* Itération dans le premier tableau */
         {
+            comparaison += 1;
+            
             min = a; /* Affection de min à la variable a */
+            
+            affectation += 2;
 
             for (int b = min; b < tableau.length; b++) /* Itération dans le deuxième tableau  */
             {
+                comparaison += 1;
+                affectation += 1;
+                
                 if (tableau[b] < tableau[min]) /* Si la valeur du tableau à l'index b est inférieur à l'index min alors */
                 {
                     min = b; /* Affectation de min au nombre minimal du tableau */
+                    
+                    comparaison += 1;
+                    affectation += 1;
                 }
             }
 
@@ -24,6 +34,9 @@ public class Main
                 temp = tableau[a]; /* Affectation de temp à l'index a du tableau */
                 tableau[a] = tableau[min]; /* Permutation entre l'index précédant et le suivant */
                 tableau[min] = temp; /* Affectation du tableau à l'index min à la valeur de temp */
+                
+                comparaison += 1;
+                affectation += 3;
             }
         }
 
@@ -31,5 +44,8 @@ public class Main
         {
             System.out.println(x); //Affichage du tableau trié
         }
+        
+        System.out.println("Affectations: " + affectation);
+        System.out.println("Comparaisons: " + comparaisons);
     }
 }
