6abe059c2f27c2259c7079002ecb81c992af84f4
Étienne <66807171+EtienneRld@users.noreply.github.com>
2021-10-21 10:11:53 +0200

    Update tri_bulle_normal.java

--- tri-bulle-normal/tri_bulle_normal.java
+++ tri-bulle-normal/tri_bulle_normal.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class TriBullesOpti
+public class tri_bulle_normal
 {
     public static void main(String[] args)
     {
@@ -15,31 +15,31 @@ public class TriBullesOpti
         {
             for (int j = 0; j < tableau.length - 1; j++) /* Itération dans le tableau complet - 1 */
             {
-                affectation += 1;
-                comparaison += 1;
-                
+                affectation += 1; /* int j = 0 || j++ */
+                comparaison += 1; /* j < tableau.length - 1;*/
+
                 if (tableau[j] > tableau[j + 1]) /* Si le nombre du 1er index est supérieur au nombre de l'index + 1 (ex: 5, 3) */
                 {
                     temp = tableau[j]; /* Affectation de tableau[j] à la variable temp (temporaire)*/
                     tableau[j] = tableau[j + 1]; /* Permutation entre le 1er index du tableau et le deuxième */
                     tableau[j + 1] = temp; /* Permutation entre le deuxième index du tableau et le 1er (temp = 1er index) */
-                    
-                    comparaison += 1;
-                    affectation += 3;
+
+                    comparaison += 1; /* tableau[j] > tableau[j + 1] */
+                    affectation += 3; /* temp = tableau[j] && tableau[j] = tableau[j + 1] && tableau[j + 1] = temp */
                 }
             }
 
             b++;
-            
-            affectation += 1;
-            comparaison += 1;
+
+            affectation += 1; /* b++ */
+            comparaison += 1; /*  while (b < tableau.length - 1) */
         }
 
         for (int x : tableau)
         {
             System.out.println(x); /* Affichage du tableau */
         }
-        
+
         System.out.println("Affectations : " + affectation);
         System.out.println("Affectations : " + comparaison);
     }
