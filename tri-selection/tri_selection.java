dbe1df3e79988bc4660f99eba22365eb4b337473
Étienne <66807171+EtienneRld@users.noreply.github.com>
2021-10-21 10:10:30 +0200

    Update tri_selection.java

--- tri-selection/tri_selection.java
+++ tri-selection/tri_selection.java
@@ -1,34 +1,33 @@
 package com.company;
 
-public class Main
+public class tri_selection
 {
     public static void main(String[] args)
     {
         int[] tableau = { 47, 12, -34, 86, -2 }; /* Déclaration du tableau */
-        int min, temp; /* Déclaration des variables */
-        
+        int min, temp;
+
         int affectation = 0;
         int comparaison = 0;
 
         for (int a = 0; a < tableau.length; a++) /* Itération dans le premier tableau */
         {
-            comparaison += 1;
-
             min = a; /* Affection de min à la variable a */
 
-            affectation += 2;
+            affectation += 2; /* int a = 0 || a ++ && min = a */
+            comparaison += 1; /* a < tableau.length */
 
             for (int b = min; b < tableau.length; b++) /* Itération dans le deuxième tableau  */
             {
-                comparaison += 1;
-                affectation += 1;
+                comparaison += 1; /* b < tableau.length */
+                affectation += 1; /* int b = min || b++ */
 
                 if (tableau[b] < tableau[min]) /* Si la valeur du tableau à l'index b est inférieur à l'index min alors */
                 {
                     min = b; /* Affectation de min au nombre minimal du tableau */
 
-                    comparaison += 1;
-                    affectation += 1;
+                    comparaison += 1; /* tableau[b] < tableau[min] */
+                    affectation += 1; /* min = b */
                 }
             }
 
@@ -38,8 +37,8 @@ public class Main
                 tableau[a] = tableau[min]; /* Permutation entre l'index précédant et le suivant */
                 tableau[min] = temp; /* Affectation du tableau à l'index min à la valeur de temp */
 
-                comparaison += 1;
-                affectation += 3;
+                affectation += 3; /* temp = tableau[a] && tableau[a] = tableau[min] && tableau[min] = temp */
+                comparaison += 1; /* a != min */
             }
         }
 
