566a1087505f18e89b42629d970a5637c8f1aef2
Maxime Nicolas <71704827+Maxime-UwU@users.noreply.github.com>
2021-10-21 09:15:16 +0200

    Update tri-bulle-opti.java

--- tri-bulle-optimisé/tri-bulle-opti.java
+++ tri-bulle-optimisé/tri-bulle-opti.java
@@ -7,6 +7,8 @@ public class TriBullesNormal
         int[] tableau = { 6, 5, 3, 1, 8, 7, 2, 4 }; /* Déclaration du tableau */
         boolean ordre = false; /* Déclaration d'un boolean qui défini la fin de l'ordre */
         int temp; /* Déclaration d'une variable pour permuter les nombres */
+        
+        affectation += 2
 
         while (!ordre) /* Tant que le tableau n'est pas trié */
         {
@@ -14,13 +16,22 @@ public class TriBullesNormal
 
             for (int j = 0; j < tableau.length - 1; j++) /* Itération dans le tableau complet - 1 */
             {
+                affectation += 1
+                comparaison += 1
+                    
                 if (tableau[j] > tableau[j + 1]) /* Si le nombre du 1er index est supérieur au nombre de l'index + 1 (ex: 5, 3) */
                 {
+                    comparaison += 1
+                        
                     ordre = false; /* Affectation de ordre à faux, le tableau n'est pas encore totalement trié */
+                    
+                    affectation += 1
 
                     temp = tableau[j]; /* Affectation de tableau[j] à la variable temp (temporaire)*/
                     tableau[j] = tableau[j + 1]; /* Permutation entre le 1er index du tableau et le deuxième */
                     tableau[j + 1] = temp; /* Permutation entre le deuxième index du tableau et le 1er (temp = 1er index) */
+                    
+                    affectation +=3
                 }
             }
         }
