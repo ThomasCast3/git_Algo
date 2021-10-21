a8ac16c84a7d1f97f743798bd1ba4e73e5b28917
Étienne <66807171+EtienneRld@users.noreply.github.com>
2021-10-21 09:20:48 +0200

    Update tri-bulle-opti.java

--- tri-bulle-optimisé/tri-bulle-opti.java
+++ tri-bulle-optimisé/tri-bulle-opti.java
@@ -8,30 +8,34 @@ public class TriBullesNormal
         boolean ordre = false; /* Déclaration d'un boolean qui défini la fin de l'ordre */
         int temp; /* Déclaration d'une variable pour permuter les nombres */
         
-        affectation += 2
+        int affectation = 0;
+        int comparaison = 0;
+
+        affectation += 2;
 
         while (!ordre) /* Tant que le tableau n'est pas trié */
         {
             ordre = true; /* Affectation de ordre à vrai tant que le tableau n'est pas trié */
+            
+            affectation += 1;
+                comparaison += 1;
 
             for (int j = 0; j < tableau.length - 1; j++) /* Itération dans le tableau complet - 1 */
             {
-                affectation += 1
-                comparaison += 1
-                    
+                affectation += 1;
+                comparaison += 1;
+                
                 if (tableau[j] > tableau[j + 1]) /* Si le nombre du 1er index est supérieur au nombre de l'index + 1 (ex: 5, 3) */
                 {
-                    comparaison += 1
                         
                     ordre = false; /* Affectation de ordre à faux, le tableau n'est pas encore totalement trié */
-                    
-                    affectation += 1
 
                     temp = tableau[j]; /* Affectation de tableau[j] à la variable temp (temporaire)*/
                     tableau[j] = tableau[j + 1]; /* Permutation entre le 1er index du tableau et le deuxième */
                     tableau[j + 1] = temp; /* Permutation entre le deuxième index du tableau et le 1er (temp = 1er index) */
                     
-                    affectation +=3
+                    affectation += 4;
+                    comparaison += 1;
                 }
             }
         }
@@ -40,5 +44,8 @@ public class TriBullesNormal
         {
             System.out.println(x); /* Affichage du tableau */
         }
+        
+        System.out.println("Affectations : " + affectation);
+        System.out.println("Comparaisons : " + comparaison);
     }
 }
